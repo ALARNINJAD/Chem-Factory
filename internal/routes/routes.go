13afea68119b346699ec5450827dfbf5f1d8a57a
Ali Rahiminejad <alarninjad@gmail.com>
2026-02-25 11:33:46 +0330

    first api request

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,11 +1,17 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"chem-factory/internal/model"
+	"chem-factory/internal/repository"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Register(server *gin.Engine) {
 	server.POST("/", root)
 	server.POST("/login", login)
-	server.POST("/register", login)
+	server.POST("/register", register)
 }
 
 func root(context *gin.Context) {
@@ -18,4 +24,15 @@ func login(context *gin.Context) {
 
 func register(context *gin.Context) {
 
+	var user model.User
+
+	if err := context.ShouldBindJSON(&user); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{})
+	}
+
+	if err := repository.SaveUser(user); err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{})
+	}
+
+	context.JSON(http.StatusCreated, gin.H{})
 }
