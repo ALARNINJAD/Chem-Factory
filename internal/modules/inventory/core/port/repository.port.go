68446ef34abbb5c00246a184450ee8f584e1255b
Ali Rahiminejad <alarninjad@gmail.com>
2026-07-10 16:47:34 +0330

    Add InventoryRepository interface for inventory management operations

--- internal/modules/inventory/core/port/repository.port.go
+++ internal/modules/inventory/core/port/repository.port.go
@@ -9,7 +9,8 @@ type InventoryRepository interface {
 	FindByID(ctx context.Context, id int) (domain.Inventory, error)
 	FindByUserID(ctx context.Context, userID int) ([]domain.Inventory, error)
 	FindIDByUserIDmatID(ctx context.Context, userID, materialID int) (int, error)
-	IncreaseByID(ctx context.Context, id, amount int) error 
+	HasByUserIDMaterialID(ctx context.Context, userID, materialID int) (bool, error)
+	IncreaseByID(ctx context.Context, id, amount int) error
 	ReduceByID(ctx context.Context, id, amount int) error
 	Add(ctx context.Context, inventory domain.Inventory) error
 	DeleteByID(ctx context.Context, id int) error
