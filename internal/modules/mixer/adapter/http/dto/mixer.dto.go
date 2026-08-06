7ce869ed14c4e4d875987337655ec596586cdc24
Ali Rahiminejad <alarninjad@gmail.com>
2026-08-06 13:59:32 +0330

    Refactor mixer DTOs to enhance structure and consistency in response types

--- internal/modules/mixer/adapter/http/dto/mixer.dto.go
+++ internal/modules/mixer/adapter/http/dto/mixer.dto.go
@@ -2,6 +2,10 @@ package dto
 
 import "time"
 
+type MixesResponse struct {
+	Mixes []MixResponse `json:"mixes"`
+}
+
 type MixRequest struct {
 	FirstIngredientID  uint `json:"first_ingredient_id"`
 	SecondIngredientID uint `json:"second_ingredient_id"`
@@ -13,20 +17,33 @@ type MixResponse struct {
 	FirstIngredientID    uint      `json:"first_ingredient_id"`
 	SecondIngredientID   uint      `json:"second_ingredient_id"`
 	Username             string    `json:"username"`
+	MaterialName         string    `json:"material_name"`
 	FirstIngredientName  string    `json:"first_ingredient_name"`
 	SecondIngredientName string    `json:"second_ingredient_name"`
 	Amount               int       `json:"amount"`
 	DateTime             time.Time `json:"date_time"`
 	RemainingSeconds     int       `json:"remaining_seconds"`
+	IsNew                bool      `json:"is_new"`
 }
 
+// response is mix response
 type CheckRequest struct {
 	ID uint `json:"id"`
 }
 
-// type CheckResponse struct {}
+type PickRequest struct {
+	ID uint `json:"id"`
+}
+type PickResponse struct {
+	IsPicked         bool `json:"is_picked"`
+	IsNew            bool `json:"is_new"`
+	RemainingSeconds int  `json:"remaining_seconds"`
+}
 
-// type GetMixesRequest struct {}
-type MixesResponse struct {
-	Mixes MixResponse `json:"mixes"`
+// response is mix response
+type NewMaterialRequest struct {
+	MixID   uint   `json:"id"`
+	Name    string `json:"name"`
+	Price   int    `json:"price"`
+	MixTime int    `json:"mix_time,omitempty"`
 }
